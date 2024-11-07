095adff62c8ed06e1a63a5bc43aa1dcb873266b1
Treybrowder <tbrowder7@gmail.com>
2024-11-07 10:41:49 -0500

    Project set up

---
+++ RecipeApp/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Trey Browder on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
